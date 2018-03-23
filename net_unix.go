43b4d3cfe4545d2be6fe30fb233c58c61ee05a13
Qian Wang <cbeuw.andy@gmail.com>
2018-03-23 16:06:55 +0000

    Callback for non TFO

--- net_unix.go
+++ net_unix.go
@@ -21,14 +21,15 @@ type TFOListener struct {
 	fd *netFD
 }
 
-func socket(family int) (int, error) {
+func socket(family int, fastOpen bool) (int, error) {
 	fd, err := syscall.Socket(family, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		return 0, err
 	}
-
-	if err := syscall.SetsockoptInt(fd, syscall.SOL_TCP, TCP_FASTOPEN, 1); err != nil {
-		return 0, err
+	if fastOpen {
+		if err := syscall.SetsockoptInt(fd, syscall.SOL_TCP, TCP_FASTOPEN, 1); err != nil {
+			return 0, err
+		}
 	}
 
 	if err := syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
@@ -39,15 +40,12 @@ func socket(family int) (int, error) {
 }
 
 func Listen(address string, fastOpen bool) (net.Listener, error) {
-	if !fastOpen {
-		return net.Listen("tcp", address)
-	}
 	laddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		return nil, err
 	}
 
-	fd, err := socket(syscall.AF_INET)
+	fd, err := socket(syscall.AF_INET, fastOpen)
 	if err != nil {
 		syscall.Close(fd)
 		return nil, err
@@ -75,15 +73,7 @@ func Listen(address string, fastOpen bool) (net.Listener, error) {
 }
 
 func Dial(address string, fastOpen bool, data []byte) (*net.TCPConn, error) {
-	if fastOpen {
-		return DialContext(context.Background(), address, data)
-	} else {
-		raddr, err := net.ResolveTCPAddr("tcp", address)
-		if err != nil {
-			return nil, err
-		}
-		return net.DialTCP("tcp", nil, raddr)
-	}
+	return DialContext(context.Background(), address, fastOpen, data)
 }
 
 var fdCallback func(int, int)
@@ -92,13 +82,13 @@ func SetFdCallback(fn func(int, int)) {
 	fdCallback = fn
 }
 
-func DialContext(ctx context.Context, address string, data []byte) (*net.TCPConn, error) {
+func DialContext(ctx context.Context, address string, fastOpen bool, data []byte) (*net.TCPConn, error) {
 	raddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		return nil, err
 	}
 
-	fd, err := socket(syscall.AF_INET)
+	fd, err := socket(syscall.AF_INET, fastOpen)
 	if err != nil {
 		syscall.Close(fd)
 		return nil, err
